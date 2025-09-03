d6acac1577c2c54051d34e5625033d63d133b0e1
Yonatan Romero <4235177+romeroyonatan@users.noreply.github.com>
2025-09-03 13:01:30 -0300

    Merge pull request #27 from Shiphero/romeroyonatan/tools/v2.0.1
    Version 2.0.1

--- pyrovider/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "2.0.0"
-
-from pyrovider.services.factories import service_provider_from_yaml
-
-__all__ = ["service_provider_from_yaml"]
---
+++ src/pyrovider/__init__.py
@@ -0,0 +1,10 @@
+import importlib.metadata
+
+from pyrovider.services.factories import service_provider_from_yaml
+
+__all__ = ["service_provider_from_yaml"]
+
+try:
+    __version__ = importlib.metadata.version("pyrovider")
+except Exception:
+    __version__ = "UNKNOWN"
