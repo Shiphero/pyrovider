2f41fc697eed88860a7850982f6a1ff3dfe16a7a
Yonatan Romero <yonatan.romero@shiphero.com>
2025-09-03 12:22:34 -0300

    Version 2.0.1

--- pyrovider/__init__.py
+++ pyrovider/__init__.py
@@ -1,5 +1,9 @@
-__version__ = "2.0.0"
-
+import importlib.metadata
 from pyrovider.services.factories import service_provider_from_yaml
 
 __all__ = ["service_provider_from_yaml"]
+
+try:
+    __version__ = importlib.metadata.version("pyrovider")
+except Exception:
+    __version__ = "UNKNOWN"
